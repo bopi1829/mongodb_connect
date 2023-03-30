29976977267af9799c4b3de13e411c3738c81d6a
bopi1829 <bopi1829@gmail.com>
2023-03-30 20:52:29 +0200

    modification connexion au server

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
 const express = require('express')
 
 const app = express()
+
+app.use((req, res, next) => {
+    res.json({ message: 'Votre requête a bien été reçue !'})
+    next()
+})
+
+module.exports = app
